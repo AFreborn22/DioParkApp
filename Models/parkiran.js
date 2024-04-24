2601508ed6916939da75425a55a643841e32b1c4
AFreborn22 <aafauzan52@gmail.com>
2024-04-25 00:10:00 +0700

    nambah manajemen parkir

---
+++ Models/parkiran.js
@@ -0,0 +1,28 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize("mysql://root@localhost:3306/parkingmonitoring")
+
+const Parkiran = sequelize.define('parkiran', {
+    blok_parkir: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    lantai: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    kendaraan: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+  });
+  
+module.exports = Parkiran;
+
