589daf2eb29f9659c7799f7bf32e293fee442d3e
AFreborn22 <aafauzan52@gmail.com>
2024-05-04 17:06:06 +0700

    the last

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use(passport.session());
 const sequelize = new Sequelize({
     dialect: 'mysql',
     username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD ,
     host: process.env.DB_HOST ,
     port: process.env.DB_PORT ,
     database: process.env.DB_NAME ,
