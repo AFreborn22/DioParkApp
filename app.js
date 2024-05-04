c40976a5781f903d7420b4cdd4b4389fbea1face
AFreborn22 <aafauzan52@gmail.com>
2024-05-04 20:20:24 +0700

    ayolah pliis

--- app.js
+++ app.js
@@ -37,23 +37,23 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// // Database Connection
-// const sequelize = new Sequelize({
-//     dialect: 'mysql',
-//     username: process.env.DB_USERNAME ,
-//     password: process.env.DB_PASSWORD ,
-//     host: process.env.DB_HOST ,
-//     port: process.env.DB_PORT ,
-//     database: process.env.DB_NAME ,
-// });
-// sequelize
-//     .authenticate()
-//     .then(() => {
-//         console.log('Connection to the database has been established successfully.');
-//     })
-//     .catch((err) => {
-//         console.error('Unable to connect to the database:', err);
-//     });
+// Database Connection
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD ,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT ,
+    database: process.env.DB_NAME ,
+});
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection to the database has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 // Routes
 app.get('/', (req, res) => (
