a68dae93295f5ce576898838a5d400460719c9a1
AFreborn22 <aafauzan52@gmail.com>
2024-05-05 20:22:24 +0700

    failure redirect google

--- Routes/auth.js
+++ Routes/auth.js
@@ -16,7 +16,7 @@ router.get('/google/callback',
   (req, res) => {
     // Jika autentikasi sukses, kirim token JWT kepada klien
     const token = jwt.sign({ id_pengguna: req.user.id_pengguna, email: req.user.email }, 'secret_key', { expiresIn: '1h' });
-    res.cookie('jwt', token, { httpOnly: true }); // Simpan token dalam cookie untuk digunakan pada setiap permintaan
+    res.cookie('token', token, { httpOnly: true }); // Simpan token dalam cookie untuk digunakan pada setiap permintaan
     res.redirect('https://diopark.vercel.app/dashboard'); // Redirect ke halaman dashboard setelah autentikasi sukses
   }
 );
