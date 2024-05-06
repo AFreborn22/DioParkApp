000db4e039635dee55b66aa023ddf1dd6e4199d5
AFreborn22 <aafauzan52@gmail.com>
2024-05-06 18:22:33 +0700

    auth google secure

--- Controllers/authGoogleController.js
+++ Controllers/authGoogleController.js
@@ -1,12 +1,10 @@
-// Controllers/authGoogleController.js
-
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Pengguna = require('../Models/pengguna');
 const jwt = require('jsonwebtoken');
 
 passport.use(new GoogleStrategy({
-    name: 'Diopark',
+    name: 'diopark app',
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.CALLBACK_URL
