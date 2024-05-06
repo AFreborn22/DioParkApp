d7195d36a8446ba486e3aba4cc5c8ccebdf51be7
AFreborn22 <aafauzan52@gmail.com>
2024-05-06 20:28:40 +0700

    auth google secure

--- helpers/generateSecretKey.js
+++ helpers/generateSecretKey.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const fs = require('fs');
 
 // Generate random bytes for secret key
-const secretKey = crypto.randomBytes(64).toString('hex');
+const secretKey = crypto.randomBytes(10).toString('hex');
 fs.writeFileSync('.env', `SECRET_KEY=${secretKey}\n`);
 
 console.log('Secret key generated and saved to .env file.');
