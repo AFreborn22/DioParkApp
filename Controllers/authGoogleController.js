571bfe56d5b98f05a0b60bd0955c7b1b9568f943
AFreborn22 <aafauzan52@gmail.com>
2024-05-06 19:38:43 +0700

    auth google secure

--- Controllers/authGoogleController.js
+++ Controllers/authGoogleController.js
@@ -55,6 +55,8 @@ const googleCallbackHandler = (req, res, next) => {
 
       res.cookie('token', token, { httpOnly: true, secure: true });
       res.status(200).send({ message: "Login berhasil", pengguna, token });
+
+      res.redirect(`${process.env.CLIENT_URL}/dashboard`);
     } catch (error) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
