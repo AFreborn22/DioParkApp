816ebeb085c79b45c2e37fcd7d1b03509f171ffc
AFreborn22 <aafauzan52@gmail.com>
2024-05-06 14:50:33 +0700

    auth google secure

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ const cors = require('cors');
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL, 
+    credentials: true, 
+  }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
