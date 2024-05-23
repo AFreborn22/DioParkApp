105cb89d6b069d72321186b0680474a72faffe81
AFreborn22 <aafauzan52@gmail.com>
2024-05-23 12:06:44 +0700

    auth google lagi

--- midleware/authMidleware.js
+++ midleware/authMidleware.js
@@ -2,20 +2,17 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
-    let token = req.headers.authorization || req.cookies.token;
-    if (!token || !token.startsWith('Bearer ')) {
+    let token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : req.cookies.token;
+    
+    if (!token) {
         return res.status(401).send({ message: "Unauthorized: No token provided" });
     }
 
-    if (token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
-    }
-
-    console.log("Received Token:", token); 
+    console.log("Received Token:", token); // Log token yang diterima
 
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
-            console.log("Token Verification Error:", err.message); 
+            console.log("Token Verification Error:", err.message); // Log kesalahan verifikasi token
             return res.status(403).send({ message: "Forbidden: Invalid token" });
         }
 
