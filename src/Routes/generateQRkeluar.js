f087ce4ac6627d7cc2faaef7ac922e4558da172c
AFreborn22 <aafauzan52@gmail.com>
2024-06-13 23:49:34 +0700

    forgot password

--- src/Routes/generateQRkeluar.js
+++ src/Routes/generateQRkeluar.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const generateQRkeluar = require('../Controllers/generateQRkeluar');
 
 // Endpoint untuk generate QR code parkiran available
-router.get('/generate-qr/exit', generateQRkeluar.generateQRCodeForExitParking);
+router.post('/generate-qr/exit', generateQRkeluar.generateQRCodeForExitParking);
 
 
 module.exports = router;
