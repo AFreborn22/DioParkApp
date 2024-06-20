be1aa1d751ad450ceb46223d289ca0b10d828c20
AFreborn22 <aafauzan52@gmail.com>
2024-06-20 10:40:48 +0700

    ubah skenario generate qr

--- src/Controllers/generateQR.js
+++ src/Controllers/generateQR.js
@@ -9,8 +9,11 @@ async function generateQRCodeForAvailableParkingMotor(req, res) {
 
         res.status(200).json({ 
             message: 'Data tempat parkir berhasil ditemukan', 
-            data: availableParking.blok_parkir 
+            data: {
+                blok_parkir: availableParking.blok_parkir
+            }
         });
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Terjadi kesalahan saat mencari tempat parkir' });
