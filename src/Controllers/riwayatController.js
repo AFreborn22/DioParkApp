71ae5b5053b982a21820ed9963fb844bdadc40b7
AFreborn22 <aafauzan52@gmail.com>
2024-06-23 21:46:47 +0700

    ayyayayayya

--- src/Controllers/riwayatController.js
+++ src/Controllers/riwayatController.js
@@ -1,9 +1,7 @@
 const Pengguna = require('../Models/pengguna');
 const Transaksi = require('../Models/transaksi');
-const { Op } = require('sequelize');
 
 exports.getRiwayatTransaksi = async (req, res) => {
-    const t = await sequelize.transaction();
     try {
         const emailPengguna = req.pengguna.email; 
 
@@ -15,25 +13,23 @@ exports.getRiwayatTransaksi = async (req, res) => {
                 attributes: ['nama', 'username', 'email', 'nomor_polisi', 'detail_kendaraan']
             }],
             attributes: ['id_transaksi', 'waktu_parkir', 'status'], 
-            order: [['waktu_parkir', 'DESC']],
-            transaction: t
+            order: [['waktu_parkir', 'DESC']] 
         });
 
         // Menghapus entri duplikat
         const uniqueTransaksi = [];
-        const transaksiMap = new Map();
+        const transaksiSet = new Set();
 
         for (const transaksi of riwayatTransaksi) {
             const key = `${transaksi.waktu_parkir}-${transaksi.status}`;
-            if (!transaksiMap.has(key)) {
-                transaksiMap.set(key, transaksi);
+            if (!transaksiSet.has(key)) {
+                transaksiSet.add(key);
                 uniqueTransaksi.push(transaksi);
             } else {
-                await Transaksi.destroy({ where: { id_transaksi: transaksi.id_transaksi }, transaction: t });
+                // Hapus transaksi duplikat dari database
+                await Transaksi.destroy({ where: { id_transaksi: transaksi.id_transaksi } });
             }
         }
-
-        await t.commit();
 
         if (uniqueTransaksi.length > 0) {
             res.send({ success: true, data: uniqueTransaksi });
@@ -41,7 +37,6 @@ exports.getRiwayatTransaksi = async (req, res) => {
             res.send({ success: false, message: "Tidak ada riwayat transaksi." });
         }
     } catch (error) {
-        await t.rollback();
         console.error("Error saat mengambil riwayat transaksi:", error);
         res.status(500).send({ success: false, message: "Terjadi kesalahan saat mengambil riwayat transaksi." });
     }
